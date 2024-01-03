02428f89153b0a22ad2a2596420562bbcde860e7
dmann <dmann@westervelt.com>
2024-01-03 15:08:34 -0600

    Added view template and CSS

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Post
+from django.template import loader
 
 # Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    context = { 'posts': posts }
+    return render(request, 'blog/post_list.html', context)
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    context = { 'post': post }
+    return render(request, 'blog/post_detail.html', context)
